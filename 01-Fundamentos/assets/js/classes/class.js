912ffc04ea930f820e0ca6b68b9750e05eed39d4
JasubiP <gameportal7@hotmail.com>
2022-11-23 23:01:57 -0600

    Set, Get, Satatic

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -1,12 +1,31 @@
 class Persona {//Se inicia una clase
-    nombre;
-    clave;
-    frase;
+    static _contador = 0;
+    static get conteo(){
+        return Persona._contador + " instancias";
+    }
+    static mensaje(){
+        console.log("Ese es un metodo estatico");
+    }
+
+    nombre = "";
+    clave = "";
+    frase = "";
+    comida = "";
 
     constructor(nombre = "Sin definir", clave = "Sin definir", frase = "Sin definir"){//Se define el contructor que dara forma al objeto
         this.nombre = nombre;
         this.clave = clave;
         this.frase = frase;
+
+        Persona._contador++;
+    }
+
+    set setComidaFavorita(comida){
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        console.log(`La comida favorita de ${this.nombre} es ${this.comida}`);
     }
 
     quienSoy(){//Metodo
@@ -14,13 +33,26 @@ class Persona {//Se inicia una clase
     }
     saludo(){
         this.quienSoy();//Se puede llamar a un metodo dentro de otro metodo
-        console.log(`${this.clave} dice: ${this.frase}`)
+        return `${this.clave} dice: ${this.frase}`;
     }
 }
 
 const spiderman = new Persona("Piter Parker", "Spiderman", "Tu fiel vecino");
-console.log(spiderman);
-const batman = new Persona("Bruce Wein", "Batman", "Soy Batman");
-console.log(batman);
+/*
 spiderman.saludo();
+spiderman.setComidaFavorita = "Pay de limon";
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
+*/
+
+const batman = new Persona("Bruce Wein", "Batman", "Soy Batman");
+/*
+batman.setComidaFavorita = "Tortas";
 batman.saludo();
+console.log(batman);
+console.log(batman.getComidaFavorita);
+*/
+
+console.log(Persona._contador);
+console.log(Persona.conteo);   
+console.log(Persona.mensaje());  
